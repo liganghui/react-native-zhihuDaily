1472a877fcd76b854b963e3a77da76f8271fe556
liganghui <1104070959@qq.com>
2019-02-21 23:00:38 +0800

    尝试重写详情页中, react-native-autoheight-webview 使用中遇到BUG

--- android/app/src/main/java/com/demo/MainApplication.java
+++ android/app/src/main/java/com/demo/MainApplication.java
@@ -3,6 +3,7 @@ package com.demo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.kishanjvaghela.cardview.RNCardViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCWebViewPackage(),
             new LinearGradientPackage(),
             new RNCardViewPackage(),
             new VectorIconsPackage(),
--- app/pages/Details/index.js
+++ app/pages/Details/index.js
@@ -8,11 +8,16 @@ import {
   Dimensions,
   WebView,
   Animated,
-  AsyncStorage
+  AsyncStorage,
+  ScrollView,
+  Image
 } from "react-native";
 import { Tile } from "react-native-elements";
 import HTML from "./html";
-
+import AutoHeightWebView from 'react-native-autoheight-webview'
+const HEADER_MAX_HEIGHT = 160;
+const HEADER_MIN_HEIGHT = 0;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 export default class index extends Component {
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state;
@@ -29,8 +34,8 @@ export default class index extends Component {
     const id = this.props.navigation.getParam("itemId");
     this.state = {
       itemId: id,
-      stories: {
-        images: []
+      news:{
+        css:[]
       }
     };
     this.props.navigation.setParams({
@@ -45,8 +50,11 @@ export default class index extends Component {
     fetch(API.details + this.state.itemId)
       .then(response => response.json())
       .then(responseJson => {
+        this.setState({
+          news:responseJson
+        })
           // 数据传输只支持字符串类型   "$R%N*D5A+F4HAA0O"  用于方便分割字符串
-        this.webview.postMessage(responseJson.image + "$R%N*D5A+F4HAA0O" +responseJson.body +"$R%N*D5A+F4HAA0O" +responseJson.image_source +"$R%N*D5A+F4HAA0O" +responseJson.title);
+        // this.webview.postMessage(responseJson.image + "$R%N*D5A+F4HAA0O" +responseJson.body +"$R%N*D5A+F4HAA0O" +responseJson.image_source +"$R%N*D5A+F4HAA0O" +responseJson.title);
         // storage.save({
         //   key: 'details', // 注意:请不要在key中使用_下划线符号!
         //   id:  this.state.itemId, // 注意:请不要在id中使用_下划线符号!
@@ -75,13 +83,65 @@ export default class index extends Component {
   };
   render() {
     return (
-      <WebView
-        ref={webview => {
-          this.webview = webview;
-        }}
-        source={{ html: HTML, baseUrl: "" }}
-        onMessage={this.onMessage}
+      <View style={styles.fill}>
+       <ScrollView
+          style={styles.fill}
+          scrollEventThrottle={16}
+       >
+      <AutoHeightWebView
+        source={{ html: this.state.news.body}}
+        files={[{
+          href: this.state.news.css[0],
+          type: 'text/css',
+          rel: 'stylesheet'
+      }]}
       />
+      </ScrollView>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1
+  },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: "#D3D3D3",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#03A9F4",
+    overflow: "hidden"
+  },
+  bar: {
+    marginTop: 28,
+    height: 32,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    backgroundColor: "transparent",
+    color: "white",
+    fontSize: 18
+  },
+  scrollViewContent: {
+    marginTop: HEADER_MAX_HEIGHT
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: null,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: 'cover',
+  },
+});
