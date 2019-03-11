8c8e55c4661bdb289cb034e23ea0cb4077ca6f3d
liganghui <1104070959@qq.com>
2019-03-12 07:13:51 +0800

    测试文件下载

--- app/pages/Test/index.js
+++ app/pages/Test/index.js
@@ -2,7 +2,7 @@
     功能测试页 , 用于新功能的调试
 */
 import React, { Component } from "react";
-import { View, Text, StyleSheet ,Button,Image} from "react-native";
+import { View, Text, StyleSheet ,Button,Image,Platform} from "react-native";
 import RNFetchBlob from "rn-fetch-blob";
 export default class index extends Component {
   constructor(props) {
@@ -11,23 +11,29 @@ export default class index extends Component {
       uri:''
     }
   }
-  imgSave(){
-    RNFetchBlob
-    .config({
-      // add this option that makes response data to be stored as a file,
-      // this is much more performant.
-      fileCache : true,
-    })
-    .fetch('GET', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552900167&di=25ee7d0026b36b1d95ca04ecc0611dd7&imgtype=jpg&er=1&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201303%2F15%2F20130315223944_EvRW3.thumb.700_0.jpeg', {
-      //some headers ..
-    })
-    .then((res) => {
-      let path=res.path();
-      this.setState({
-        uri:path
-      })
-      console.warn('保存的图片路径 ',path)
-    })
+    downloadImage(){
+      var date      = new Date();
+      var url       = "https://pic3.zhimg.com/v2-2e3aaa3b1b34273062e5a06822a984ce.jpg'";
+      var ext       = this.getExtention(url);
+      ext = "."+ext[0];
+      const { config, fs } = RNFetchBlob ; 
+      let PictureDir = fs.dirs.PictureDir
+      let options = {
+      fileCache: true,
+       addAndroidDownloads : {
+         useDownloadManager : true,
+         notification : true,
+         path:  PictureDir + "/image_6666666",
+        description : 'Image'
+         }
+      }
+       config(options).fetch('GET', url).then((res) => {
+         Alert.alert("Download Success !");
+      });
+   }
+     getExtention(filename){
+       return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : 
+   undefined;
   }
   render() {
     return(
@@ -35,8 +41,8 @@ export default class index extends Component {
       <Text>功能测试页</Text>
       <Text>当前测试项: 图片保存</Text>
       <Text>保存的图片:</Text>
-      <Image source={{uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552900167&di=25ee7d0026b36b1d95ca04ecc0611dd7&imgtype=jpg&er=1&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201303%2F15%2F20130315223944_EvRW3.thumb.700_0.jpeg'}} style={{width:400,height:300}}/>
-      <Button onPress={this.imgSave.bind(this)} title="保存图片"/>
+      <Image source={{uri:'https://pic3.zhimg.com/v2-2e3aaa3b1b34273062e5a06822a984ce.jpg'}} style={{width:400,height:300}}/>
+      <Button onPress={this.downloadImage.bind(this)} title="保存图片"/>
       <Text>{this.state.uri}</Text>
     </View>
     )
