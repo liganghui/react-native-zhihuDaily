e18491c6a46da2e079dcf2d1d2d0208ccfb38f7b
liganghui <1104070959@qq.com>
2019-03-12 07:30:08 +0800

    图像保存测试

--- app/pages/Test/index.js
+++ app/pages/Test/index.js
@@ -2,7 +2,7 @@
     功能测试页 , 用于新功能的调试
 */
 import React, { Component } from "react";
-import { View, Text, StyleSheet ,Button,Image,Platform} from "react-native";
+import { View, Text, StyleSheet ,Button,Image,Platform,ToastAndroid,CameraRoll} from "react-native";
 import RNFetchBlob from "rn-fetch-blob";
 export default class index extends Component {
   constructor(props) {
@@ -11,29 +11,35 @@ export default class index extends Component {
       uri:''
     }
   }
-    downloadImage(){
-      var date      = new Date();
-      var url       = "https://pic3.zhimg.com/v2-2e3aaa3b1b34273062e5a06822a984ce.jpg'";
-      var ext       = this.getExtention(url);
-      ext = "."+ext[0];
-      const { config, fs } = RNFetchBlob ; 
-      let PictureDir = fs.dirs.PictureDir
-      let options = {
-      fileCache: true,
-       addAndroidDownloads : {
-         useDownloadManager : true,
-         notification : true,
-         path:  PictureDir + "/image_6666666",
-        description : 'Image'
-         }
-      }
-       config(options).fetch('GET', url).then((res) => {
-         Alert.alert("Download Success !");
-      });
-   }
-     getExtention(filename){
-       return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : 
-   undefined;
+  saveToCameraRoll = () => {
+    const{selectedImage, downlaodUrl} = this.state
+    let url ='https://pic3.zhimg.com/v2-2e3aaa3b1b34273062e5a06822a984ce.jpg';
+    ToastAndroid.show("Image is Saving...", ToastAndroid.SHORT)
+    if (Platform.OS === 'android'){ 
+      RNFetchBlob
+        .config({
+          fileCache : true,
+          appendExt : 'jpg'
+        })
+        .fetch('GET', url)
+        .then((res) => {
+            console.log()
+         CameraRoll.saveToCameraRoll(res.path())
+
+            .then((res) => {
+             console.log("save", res)
+             ToastAndroid.show("Image saved Successfully.", ToastAndroid.SHORT)
+            }).catch((error) => {
+                ToastAndroid.show("Ops! Operation Failed", ToastAndroid.SHORT)
+
+            })
+
+        })
+    } else {
+        CameraRoll.saveToCameraRoll(url)
+          .then(alert('Success', 'Photo added to camera roll!'))
+          ToastAndroid.show("Image saved Successfully.", ToastAndroid.SHORT)
+    }
   }
   render() {
     return(
@@ -42,7 +48,7 @@ export default class index extends Component {
       <Text>当前测试项: 图片保存</Text>
       <Text>保存的图片:</Text>
       <Image source={{uri:'https://pic3.zhimg.com/v2-2e3aaa3b1b34273062e5a06822a984ce.jpg'}} style={{width:400,height:300}}/>
-      <Button onPress={this.downloadImage.bind(this)} title="保存图片"/>
+      <Button onPress={this.saveToCameraRoll.bind(this)} title="保存图片"/>
       <Text>{this.state.uri}</Text>
     </View>
     )
