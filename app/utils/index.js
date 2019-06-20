17de02de2e7e0dad6275b89c67f339c530dc3705
liganghui <1104070959@qq.com>
2019-06-20 09:05:00 +0800

    重置依赖

--- app/utils/index.js
+++
@@ -1,5 +0,0 @@
-    import { Axios } from './axios';
-    import { Api } from './api';
-    import { Tools,Px2Dp } from './tools';
-    import { System } from './system';
-    export { Axios,Api,Tools,System,Px2Dp};
